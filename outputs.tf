9fd6714db70899a464d057772a0d1b79edd9c9d7
Rael Garcia <rael@redhat.com>
2021-09-21 14:00:18 +0200

    feat: enable replica bucket kms encryption

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,16 @@ output "kms_master_bucket_key_id" {
   value       = aws_kms_key.master.key_id
   description = "The KMS bucket key id."
 }
+
+output "s3_replica_bucket_id" {
+  value = module.replica.s3_bucket_id
+}
+
+output "s3_replica_bucket_arn" {
+  value = module.replica.s3_bucket_arn
+}
+
+output "kms_replica_bucket_key_id" {
+  value       = aws_kms_key.replica.key_id
+  description = "The KMS bucket key id."
+}
