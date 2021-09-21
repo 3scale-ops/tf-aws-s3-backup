b9dad605eb2e6a50ee06b1500fb081d603766eef
Rael Garcia <rael@redhat.com>
2021-09-21 13:16:47 +0200

    clean: group master bucket resources

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "s3_bucket_id" {
-  value = module.bucket.s3_bucket_id
+output "s3_master_bucket_id" {
+  value = module.master.s3_bucket_id
 }
 
-output "s3_bucket_arn" {
-  value = module.bucket.s3_bucket_arn
+output "s3_master_bucket_arn" {
+  value = module.master.s3_bucket_arn
 }
 
-output "kms_bucket_key_id" {
-  value       = aws_kms_key.bucket.key_id
+output "kms_master_bucket_key_id" {
+  value       = aws_kms_key.master.key_id
   description = "The KMS bucket key id."
 }
